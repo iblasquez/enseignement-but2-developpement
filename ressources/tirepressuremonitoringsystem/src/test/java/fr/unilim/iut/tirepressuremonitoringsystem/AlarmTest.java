5487e17c8518b3db915e3260d69d2567ac683dcb
Isabelle BLASQUEZ <isabelle.blasquez31@gmail.com>
2022-09-16 11:03:30 +0200

    ajout ressources tirepressuremonitoringsystem

---
+++ ressources/tirepressuremonitoringsystem/src/test/java/fr/unilim/iut/tirepressuremonitoringsystem/AlarmTest.java
@@ -0,0 +1,13 @@
+package fr.unilim.iut.tirepressuremonitoringsystem;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+class AlarmTest {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
