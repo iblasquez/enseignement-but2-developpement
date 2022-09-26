47ae1ca2d310fbbeb11e3581a0f24a12f7c1e0de
Isabelle BLASQUEZ <isabelle.blasquez31@gmail.com>
2022-09-26 09:56:52 +0200

    ajout ressources du kata Parrot

---
+++ ressources/kataparrot/src/test/java/fr/unilim/iut/kataparrot/ParrotTest.java
@@ -0,0 +1,50 @@
+package fr.unilim.iut.kataparrot;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class ParrotTest {
+
+	@Test
+	void getSpeedOfEuropeanParrot() {
+		Parrot parrot = new Parrot(ParrotTypeEnum.EUROPEAN, 0, 0, false);
+		assertEquals(12.0, parrot.getSpeed(), 0.0);
+	}
+
+	@Test
+	void getSpeedOfAfricanParrot_With_One_Coconut() {
+		Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 1, 0, false);
+		assertEquals(3.0, parrot.getSpeed(), 0.0);
+	}
+
+	@Test
+	void getSpeedOfAfricanParrot_With_Two_Coconuts() {
+		Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 2, 0, false);
+		assertEquals(0.0, parrot.getSpeed(), 0.0);
+	}
+
+	@Test
+	void getSpeedOfAfricanParrot_With_No_Coconuts() {
+		Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 0, 0, false);
+		assertEquals(12.0, parrot.getSpeed(), 0.0);
+	}
+
+	@Test
+	void getSpeedNorwegianBlueParrot_nailed() {
+		Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 0, true);
+		assertEquals(0.0, parrot.getSpeed(), 0.0);
+	}
+
+	@Test
+	void getSpeedNorwegianBlueParrot_not_nailed() {
+		Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 1.5, false);
+		assertEquals(18.0, parrot.getSpeed(), 0.0);
+	}
+
+	@Test
+	void getSpeedNorwegianBlueParrot_not_nailed_high_voltage() {
+		Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 4, false);
+		assertEquals(24.0, parrot.getSpeed(), 0.0);
+	}
+}
